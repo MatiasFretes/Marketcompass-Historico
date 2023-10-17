4662a68bfdd87bf36e7f9c2ac9bd83c1244e34f6
unknown <maty.fretes10@gmail.com>
2023-10-17 01:25:38 -0300

    Se agrega CA5 y CA6 a US5

--- lib/src/test/java/US5.java
+++ lib/src/test/java/US5.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import extensible.FiltradorPorCriterio;
@@ -10,11 +11,14 @@ import service.HistoricoRecomendacionesService;
 import observable.RecomendadorObservable;
 import org.mockito.Mockito;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -23,7 +27,7 @@ public class US5 {
 	private Core core;
 	private FiltradorPorCriterio criterio;
 	private ObservadorDeRecomendaciones observadorMock;
-    private HistoricoRecomendacionesService service;
+    private static HistoricoRecomendacionesService service;
     private DB_RecomendacionRepository repositorioMock;
 	private String rutaJsonMercados = "src/test/resources/mercados.json";
 	private String rutaJarCriterio = "src/test/resources/Distancia.jar";
@@ -31,9 +35,11 @@ public class US5 {
 	private List<String> productoInexistente = Arrays.asList("NoExiste");  
 	private DB_Recomendacion recomendacionValida = new DB_Recomendacion("M1", Arrays.asList("P1"), "C1");
 	private DB_Recomendacion recomendacionInvalida = new DB_Recomendacion("", Arrays.asList(""), "");
-	
+	private List<DB_Recomendacion> db_recomendaciones = new ArrayList<>();
+	private String criterioValido = "C1";
+	private List<String> productosRecomendados = Arrays.asList("P1", "P2");
+	private List<String> productosNoRecomendados = Arrays.asList("P1", "P3");
 
-    @SuppressWarnings("deprecation")
 	@BeforeEach
     void setUp() {
         observadorMock = Mockito.mock(ObservadorDeRecomendaciones.class);
@@ -44,10 +50,15 @@ public class US5 {
         core.recomendador.recomendadorObservable.addObserver(observadorMock); 
         criterio = core.criterios.stream().findFirst().get();
         repositorioMock = mock(DB_RecomendacionRepository.class);
-        service = new HistoricoRecomendacionesService();
         service.setRepository(repositorioMock);
+        db_recomendaciones.addAll(Arrays.asList(new DB_Recomendacion("M1", Arrays.asList("P1", "P2"), "C1"), new DB_Recomendacion("M2", Arrays.asList("P2", "P3"), "C2")));
     }
-
+    
+    @BeforeAll
+    static void setUpService() {
+        service = new HistoricoRecomendacionesService();
+    }
+    
     @Test
     void CA1_ObservarNuevaRecomendacion(){
         core.obtenerRecomendacion(criterio, productoExistente);
@@ -72,5 +83,23 @@ public class US5 {
         assertFalse(recomendacionInvalida.esValida());
         service.insertarRecomendacion(recomendacionInvalida);
         verify(repositorioMock, never()).insert(recomendacionInvalida);
+    }
+    
+    @Test
+    void CA5_consultarRecomendacionExistente() {
+        when(service.consultarTodasRecomendaciones()).thenReturn(db_recomendaciones);
+        List<DB_Recomendacion> recomendaciones = service.consultarRecomendacionesPorProductosYCriterio(productosRecomendados, criterioValido);
+        assertFalse(recomendaciones.isEmpty());
+        recomendaciones.forEach(recomendacion -> { assertTrue(recomendacion.getPeticionUsuario().containsAll(productosRecomendados) 
+        			&& recomendacion.getPeticionUsuario().size() == productosRecomendados.size() 
+        			&& criterioValido.equals(recomendacion.getCriterioNombre()));
+        });
+    }
+    
+    @Test
+    void CA6_consultarRecomendacionInexistente() {
+        when(service.consultarTodasRecomendaciones()).thenReturn(db_recomendaciones);
+        List<DB_Recomendacion> recomendaciones = service.consultarRecomendacionesPorProductosYCriterio(productosNoRecomendados, criterioValido);
+        assertTrue(recomendaciones.isEmpty());
     }
 }
